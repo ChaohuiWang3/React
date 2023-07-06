f0c03d15544e5a0d937495c029ff75353db9fcd1
ChaohuiWang3 <wch3145863356@gmail.com>
2023-07-06 19:13:26 +0800

    background and logo image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-import styles from './Button.module.scss'; // Import regular stylesheet
+import backgroundstyle from './background.module.scss';
+import poe from './poe.png'
+import chatgpt from './chatgpt.png'
+console.log(poe, chatgpt);
 
 class App extends Component {
    render() {
-    return <button className = {styles.error}>Error Button</button>;
+    return(
+      <div className={backgroundstyle.background}>   {/*背景*/}
+        <div className={backgroundstyle.topcover}>    {/*顶端遮罩*/}
+          <div className={backgroundstyle.poe_img}> <img src={poe} alt = "poe logo"/> </div> {/*poe顶端logo图片*/}
+          <div className={backgroundstyle.poe_title}>Poe</div>  {/*poe标题*/}
+          <div className={backgroundstyle.chatgpt_img}> <img src={chatgpt} alt = "chatgpt logo"/> </div> {/*chatgpt顶端logo图片*/} 
+          <div className={backgroundstyle.chatgpt_title}>ChatGPT</div>  {/*chatgpt标题*/}
+        </div>
+
+        
+        <div className={backgroundstyle.leftcover}> {/*左侧遮罩*/}
+          <div className={backgroundstyle.explore_title}>Explore</div>
+        </div>
+      </div>
+      
+      
+    )
   }
-}
+} 
 
 export default App 
