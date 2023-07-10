8d6be61e648d9b74d301c0c80b2495785d6a8f95
ChaohuiWang3 <wch3145863356@gmail.com>
2023-07-10 19:55:01 +0800

    finish webpage layout style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import background from './scene/background.module.scss';
 import toppart from './scene/toppart.module.scss';
 import leftpart from './scene/leftpart.module.scss';
+import midpart from './scene/midpart.module.scss';
 
 //输入图片
 import poe from './image/poe.png';
@@ -22,32 +23,45 @@ import subscribe from './image/subscribe.png';
 import profile from './image/profile.png';
 import settings from './image/settings.png';
 import send_feedback from './image/send_feedback.png';
-
+import share from './image/share.png';
+import clear_context from './image/clear_context.png';
+import voice from './image/voice.png';
+import input_button from './image/input_button.png';
 
 class App extends Component {
    render() {
     return(  
       <div className={background.background}>         {/*背景*/}
+
+        {/*顶端模块*/}
         <div className={toppart.topcover}>            {/*顶端遮罩*/} 
-          <div className={toppart.poe_button}>        {/*左上角poe模块*/}
+          <div className={toppart.poe}>        {/*左上角poe模块*/}
             <span>Poe</span>
             <img src={poe} alt="poe"/>
           </div>
-          <div className={toppart.chatgpt_button}>    {/*正上方chatgpt模块*/}
+
+          <div className={toppart.chatgpt}>    {/*正上方chatgpt模块*/}
             <img src={chatgpt} alt="chatgpt"/>
             <span>ChatGPT</span>
             <img src={chatgpt_arrow} alt="chatgpt_arrow"/>
           </div> 
+
+          <div className={toppart.share}>    {/*正上方share模块*/}
+            <img src={share} alt="share"/>
+            <span>Share</span>
+          </div> 
         </div>
+     
       
+        {/*左侧模块*/}
         <div className={leftpart.leftcover}>          {/*左侧遮罩*/}
-          <div className={leftpart.explore_button}>   {/*左侧explore模块*/}
+          <div className={leftpart.explore}>   {/*左侧explore模块*/}
             <img src={explore} alt="explore"/>    
             <span>Explore</span>
             <img src={explore_arrow} alt="explore_arrow"/>  
           </div>
 
-          <div className={leftpart.create_bot_button}> {/*左侧create_bot模块*/}
+          <div className={leftpart.create_bot}> {/*左侧create_bot模块*/}
             <img src={create_bot} alt="create_bot"/>
             <span>Create bot</span>
             <img src={create_bot_plus} alt="create_bot_plus"/>
@@ -55,7 +69,7 @@ class App extends Component {
 
           <div className={leftpart.sage}>  {/*左侧sage模块*/}
             <img src={sage} alt="sage"/>
-            <span>Sage</span>
+            <span>Sage</span> 
           </div>
 
           <div className={leftpart.gpt4}>  {/*左侧列表gpt4模块*/}
@@ -64,6 +78,7 @@ class App extends Component {
             <second-span>Limited access</second-span>
           </div>
 
+        
           <div className={leftpart.claude}>  {/*左侧列表claude+模块*/}
             <img src={claude} alt="claude"/>
             <first-span>Claude+</first-span>
@@ -111,11 +126,29 @@ class App extends Component {
             <span>Send Feedback</span>
           </div>
 
-          <div className={leftpart.download_ios_app}>  {/*左侧列表download_ios_app模块*/}
-            <button className={leftpart.download_ios_app_inner}>
+          <div className={leftpart.download_space}>    {/*左侧列表下载空间*/}
+            <button className={leftpart.download_ios_app}>   {/*左侧列表下载空间内download_ios_app模块*/}
               <span>Download iOS app</span>
             </button>
           </div>
+
+          <div className={leftpart.scroll_bar_space}>  {/*左侧列表滚动条空间*/}
+            <div className={leftpart.scroll_bar}></div>    {/*左侧列表滚动条模块*/}
+          </div>
+        </div>
+
+
+        {/*输入模块*/}
+        <img className={midpart.clear_context} src={clear_context} alt="clear_context"/> {/*清除按钮*/}
+
+        <div className={midpart.input}>   {/*输入框*/}
+          <span>Talk to ChatGPT on Poe</span>
+        </div>   
+
+        <img className={midpart.voice} src={voice} alt='voice'/>   {/*语音*/}
+
+        <div className={midpart.input_button}>     {/*输入按钮*/}
+          <img src={input_button} alt="input_button"/>
         </div>
       </div>   
     )
